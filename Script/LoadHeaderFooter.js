d222e5d2990a1b410761ac8d908b5a89ccd9d2e0
tyler <tyler@lutogames.com>
2017-08-19 18:18:23 -0400

    fixed footer responsive; fixed hamburger positioning; removed some uneccessary flexbox;

--- Script/LoadHeaderFooter.js
+++ Script/LoadHeaderFooter.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-    $("#header").load("./HTML-Chunks/Header.html");
+    $("#header").load("./HTML-Chunks/Header.html", headerDidLoad);
 
     $("#footer").load("./HTML-Chunks/Footer.html");
 });
+
+function headerDidLoad(){
+    $("#hamburgerIcon").click(function() {
+        console.log('click registered');
+
+        $("#headerNavLinksContainer").css("display", "block");
+    });
+}
